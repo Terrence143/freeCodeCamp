da9e1b0b206df692bea77ad2c2943d1ee8b4cf65
Terrence <40508730+Terrence143@users.noreply.github.com>
2019-03-14 17:58:00 +0800

    updated

--- app/main/config.py
+++ app/main/config.py
@@ -1,7 +1,7 @@
 import os
 
 # uncomment the line below for postgres database url from environment variable
-postgres_local_base = 'postgresql://postgres:password@localhost5432/RESTplus'
+postgres_local_base = 'postgresql://postgres:password@localhost:5432/RESTplus'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,14 +14,14 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost5432/RESTplus'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/RESTplus'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost5432/RESTplus'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/RESTplus'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
