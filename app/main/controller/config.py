efb04d982725c421e971a7536768fe9f418f28cf
Terrence <40508730+Terrence143@users.noreply.github.com>
2019-03-14 14:28:22 +0800

    updated

---
+++ app/main/controller/config.py
@@ -0,0 +1,41 @@
+import os
+
+# uncomment the line below for postgres database url from environment variable
+mysql_local_base = os.environ['DATABASE_URL']
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'password')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/restplus'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/restplus'
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
