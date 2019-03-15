e660eb7b89a1f24bf16f6555cd6588229cc289e8
Terrence <40508730+Terrence143@users.noreply.github.com>
2019-03-15 12:24:01 +0800

    updated bug

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ from app.main.model import user
 
 from app.main import create_app, db
 
-app = create_app(os.getenv('dev')
+app = create_app('dev')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
